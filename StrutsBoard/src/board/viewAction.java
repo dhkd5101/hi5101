523cbbec14fcd08f95c00bf0ad6afa646cd4f6bc
asd-PC\asd <sbksuk@naver.com>
2018-05-16 20:57:06 +0900

    내꺼에올리기

---
+++ StrutsBoard/src/board/viewAction.java
@@ -0,0 +1,27 @@
+package board;
+
+import java.io.InputStream;
+import java.io.Reader;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class viewAction extends ActionSupport{
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+	
+	private boardVO paramClass = new boardVO();
+	private boardVO resultClass = new boardVO();
+	
+	private int currentPage;
+	
+	private int no;
+	private String password;
+	
+	private String fileUploadPath = "C:/java/upload/";
+	
+	private InputStream inputStream;
+	private String contentDisposition;
+	
+
+}
